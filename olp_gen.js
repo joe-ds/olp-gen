e83a3079837ebf7ca22dbeae4e29e754f44ee189
joe-ds <jdsouza@nettitude.com>
2024-03-19 17:02:06 +0000

    Moved from old repo [last commit Apr 24, 2019 ]

---
+++ olp_gen.js
@@ -0,0 +1,101 @@
+welcome = "Welcome!"; // Set a default welcome message.
+output = [];
+
+function generate(text, i) {
+    output[i] = text;
+    f_output = output.join('\n[===]\n');
+    document.getElementById("output").value = f_output;
+}
+
+function handle_osj(text) {
+    osj = JSON.parse(text);
+    var s = [];
+    for (i = 1; i < osj.length; i++) {
+        o = osj[i];
+        if (!o.serviceitem) {
+            continue;
+        } else if (o.serviceitem.header.name == "songs") {
+            t = o.serviceitem.header.title;
+            s.push(t.replace(/<.*>/g, ""));
+        }
+    }
+    generate(s.join("\n\n"), 2);
+}
+
+function speaker(evt) {
+    s = evt.target.value;
+    if (s) {
+        generate(s, 1);
+    }
+}
+
+function scripture(evt) {
+    book = document.getElementById("book").value;
+    chap = document.getElementById("chapter").value.toString();
+    verse = document.getElementById("verse").value.toString();
+    speaker_name = document.getElementById("speaker").value;
+    if (book == "unknown") {
+        text = null;
+        generate(speaker_name, 1);
+    } else { 
+        text = book + " " + chap + ":" + verse;
+        generate(speaker_name + "\n\n" + text, 1);
+    }
+}
+
+function handleFile(evt) {
+    f = evt.target.files;
+    z = f[0];
+    zip.createReader(new zip.BlobReader(z), function(reader) {
+        // get all entries from the zip
+        reader.getEntries(function(entries) {
+            if (entries.length) {
+                search: for (i = 0; i < entries.length; i++) {
+                    if (entries[i].filename.slice(-4) == ".osj") {
+                        // get first entry content as text
+                        entries[0].getData(new zip.TextWriter(), function(text) {
+                        // text contains the entry data as a String
+                        
+                        // document.getElementById("test").innerHTML = text;
+                        handle_osj(text);
+            
+                        // close the zip reader
+                        reader.close(function() {
+                        // onclose callback
+                        });
+            
+                        }, function(current, total) {
+                        // onprogress callback
+                        });
+                        
+                        break search;
+                    }
+                }
+            }
+          });
+        }, function(error) {
+          // onerror callback
+          document.getElementById("output").value = 'Unexpected error';
+        });
+    }
+
+window.onload = function() {
+    if (window.File && window.Blob) {
+        output = [welcome, document.getElementById("speaker").value]
+        var x = document.getElementById("file").addEventListener("change", handleFile, false);
+        var y = document.getElementById("speaker").addEventListener("change", speaker, false);
+        var z = document.getElementById("scripture").addEventListener("change", scripture, false);
+        document.getElementById("copy").addEventListener("click", function(evt) {
+            var x = document.getElementById("output");
+            x.select();
+
+            try {
+                document.execCommand('copy');
+            } catch (err) {
+                evt.target.disabled;
+            }
+        });
+    } else {
+        document.getElementById("output").value = 'APIs unsupported';
+    }
+};
